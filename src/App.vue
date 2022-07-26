ebf5db8af5d653987fc82c6ed1f684599fa7e4b2
Andrey Provkov <provkovasu15@gmail.com>
2022-07-26 14:46:57 +0500

    Add table employees with sort by table headers'

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,18 @@
 <template>
   <v-app>
     <v-main>
+      <ListEmployee />
     </v-main>
   </v-app>
 </template>
 
 <script>
+import ListEmployee from "@/components/ListEmployee.vue"
+
 export default {
   name: 'App',
   components: {
+    ListEmployee
   },
   data: () => ({
     //
---
+++ src/components/ListEmployee.vue
@@ -0,0 +1,85 @@
+<template>
+  <v-data-table
+    :headers="headers"
+    :items="employees"
+    hide-default-footer
+    class="elevation-1"
+  ></v-data-table>
+</template>
+
+<script>
+export default {
+  name: 'ListEmployee',
+  data () {
+    return {
+      headers: [
+        { text: 'Имя', value: 'firstname' },
+        { text: 'Фамилия', value: 'surname' },
+        { text: 'Отчество', value: 'patronymic' },
+        { text: 'Должность', value: 'jobtitle' }
+      ],
+      employees: [
+        {
+          id: 1,
+          firstname: 'Эстелла',
+          surname: 'Щербакова',
+          patronymic: 'Борисовна',
+          jobtitle: 'Менеджер'
+        },
+        {
+          id: 2,
+          firstname: 'Иоанна',
+          surname: 'Красильникова',
+          patronymic: 'Христофоровна',
+          jobtitle: 'Бухгалтер'
+        },
+        {
+          id: 3,
+          firstname: 'Марфа',
+          surname: 'Мэлсовна',
+          patronymic: 'Мельникова',
+          jobtitle: 'Диспетчер'
+        },
+        {
+          id: 4,
+          firstname: 'Мэри',
+          surname: 'Субботина',
+          patronymic: 'Антониновна',
+          jobtitle: 'Психолог'
+        },
+        {
+          id: 5,
+          firstname: 'Аким',
+          surname: 'Марков',
+          patronymic: 'Куприянович',
+          jobtitle: 'Механик'
+        },
+        {
+          id: 6,
+          firstname: 'Лев',
+          surname: 'Субботин',
+          patronymic: 'Львович',
+          jobtitle: 'Инженер'
+        },
+        {
+          id: 7,
+          firstname: 'Валерий',
+          surname: 'Коновалов',
+          patronymic: 'Геннадиевич',
+          jobtitle: 'Брокер'
+        },
+        {
+          id: 8,
+          firstname: 'Артур',
+          surname: 'Ситников',
+          patronymic: 'Лукьевич',
+          jobtitle: 'Аналитик'
+        }
+      ],
+    }
+  }
+}
+</script>
+
+<style>
+</style>
