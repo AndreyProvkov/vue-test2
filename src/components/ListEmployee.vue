e1888b16831010124827a5e53d10576385938102
Andrey Provkov <provkovasu15@gmail.com>
2022-07-26 20:26:04 +0500

    Add fieldes to employees

--- src/components/ListEmployee.vue
+++ src/components/ListEmployee.vue
@@ -85,6 +85,66 @@
                       label="Должность"
                     ></v-text-field>
                   </v-col>
+                  <v-col
+                    cols="12"
+                    sm="6"
+                    md="4"
+                  >
+                    <v-checkbox
+                      v-model="editedEmployer.workBookSubmitted"
+                      :label="`Трудовая книжка сдана`"
+                    ></v-checkbox>
+                  </v-col>
+                  <v-col
+                    cols="12"
+                    sm="6"
+                    md="4"
+                  >
+                    <v-text-field
+                      v-model.number="editedEmployer.salary"
+                      label="Оклад"
+                    ></v-text-field>
+                  </v-col>
+                  <v-col
+                    cols="12"
+                    sm="6"
+                    md="4"
+                  >
+                    <v-menu
+                      v-model="menuDate"
+                      :close-on-content-click="false"
+                      :nudge-right="40"
+                      transition="scale-transition"
+                      offset-y
+                      min-width="auto"
+                    >
+                      <template v-slot:activator="{ on, attrs }">
+                        <v-text-field
+                          v-model="editedEmployer.startDate"
+                          label="Дата выхода на работу"
+                          prepend-icon="mdi-calendar"
+                          readonly
+                          v-bind="attrs"
+                          v-on="on"
+                        ></v-text-field>
+                      </template>
+                      <v-date-picker
+                        v-model="editedEmployer.startDate"
+                        @input="menuDate = false"
+                      ></v-date-picker>
+                    </v-menu>
+                  </v-col>
+                  <v-col
+                    cols="12"
+                    sm="6"
+                    md="4"
+                  >
+                    <v-select
+                      v-model.number="editedEmployer.time"
+                      :items="times"
+                      label="Ставка"
+                    ></v-select>
+                  </v-col>
                 </v-row>
               </v-container>
             </v-card-text>
@@ -157,6 +217,8 @@ export default {
       search: '',
       dialog: false,
       dialogDelete: false,
+      menuDate: false,
+      times: ['Полная', 'Половина'],
       headers: [
         { text: 'Имя', value: 'firstname' },
         { text: 'Фамилия', value: 'surname' },
@@ -175,13 +237,21 @@ export default {
         firstname: '',
         surname: '',
         patronymic: '',
-        jobtitle: ''
+        jobtitle: '',
+        workBookSubmitted: false,
+        salary: 0,
+        startDate: "",
+        time: ''
       },
       defaultEmployer: {
         firstname: '',
         surname: '',
         patronymic: '',
-        jobtitle: ''
+        jobtitle: '',
+        workBookSubmitted: false,
+        salary: 0,
+        startDate: "",
+        time: ''
       },
     }
   },
@@ -208,60 +278,84 @@ export default {
     initialize () {
       this.employees = [
         {
-          id: 1,
           firstname: 'Эстелла',
           surname: 'Щербакова',
           patronymic: 'Борисовна',
-          jobtitle: 'Менеджер'
+          jobtitle: 'Менеджер',
+          workBookSubmitted: true,
+          salary: 20000,
+          startDate: "2022-09-10",
+          time: 'Полная'
         },
         {
-          id: 2,
           firstname: 'Иоанна',
           surname: 'Красильникова',
           patronymic: 'Христофоровна',
-          jobtitle: 'Бухгалтер'
+          jobtitle: 'Бухгалтер',
+          workBookSubmitted: true,
+          salary: 25000,
+          startDate: "2022-08-10",
+          time: 'Половина'
         },
         {
-          id: 3,
           firstname: 'Марфа',
           surname: 'Мэлсовна',
           patronymic: 'Мельникова',
-          jobtitle: 'Диспетчер'
+          jobtitle: 'Диспетчер',
+          workBookSubmitted: false,
+          salary: 15000,
+          startDate: "2022-09-15",
+          time: 'Половина'
         },
         {
-          id: 4,
           firstname: 'Мэри',
           surname: 'Субботина',
           patronymic: 'Антониновна',
-          jobtitle: 'Психолог'
+          jobtitle: 'Психолог',
+          workBookSubmitted: false,
+          salary: 23000,
+          startDate: "2022-08-01",
+          time: 'Полная'
         },
         {
-          id: 5,
           firstname: 'Аким',
           surname: 'Марков',
           patronymic: 'Куприянович',
-          jobtitle: 'Механик'
+          jobtitle: 'Механик',
+          workBookSubmitted: true,
+          salary: 16000,
+          startDate: "2022-07-03",
+          time: 'Половина'
         },
         {
-          id: 6,
           firstname: 'Лев',
           surname: 'Субботин',
           patronymic: 'Львович',
-          jobtitle: 'Инженер'
+          jobtitle: 'Инженер',
+          workBookSubmitted: true,
+          salary: 40000,
+          startDate: "2022-10-10",
+          time: 'Полная'
         },
         {
-          id: 7,
           firstname: 'Валерий',
           surname: 'Коновалов',
           patronymic: 'Геннадиевич',
-          jobtitle: 'Брокер'
+          jobtitle: 'Брокер',
+          workBookSubmitted: false,
+          salary: 35000,
+          startDate: "2022-06-20",
+          time: 'Полная'
         },
         {
-          id: 8,
           firstname: 'Артур',
           surname: 'Ситников',
           patronymic: 'Лукьевич',
-          jobtitle: 'Аналитик'
+          jobtitle: 'Аналитик',
+          workBookSubmitted: false,
+          salary: 20000,
+          startDate: "2022-09-10",
+          time: 'Половина'
         }
       ]
     },
